2846955220b8d540ccdf7bbc6ff566c725cf0224
Alice <alice.qian.mail@gmail.com>
2023-10-10 00:09:02 -0400

    updated routers for auth

--- movie-rater/src/index.js
+++ movie-rater/src/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Auth from './components/auth';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Auth />
+  },
+  {
+    path: "/movies",
+    element: <App />
+  }
+])
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
