b63d6d26fd775b7083612e185b8ce7361f57f085
Alice <alice.qian.mail@gmail.com>
2023-10-10 17:07:32 -0400

    added authentication login page

--- movie-rater/src/api-service.js
+++ movie-rater/src/api-service.js
@@ -34,4 +34,15 @@ export class API {
     		}
 		})
 	}
+
+	static loginUser(body) {
+		return fetch(`http://127.0.0.1:8000/auth/`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+    		},
+            body: JSON.stringify(body)
+		})
+		.then( reps => reps.json())
+	}
 }
