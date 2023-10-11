5ed62e4131921003f559b3802fe594f5b8452fbb
Alice <alice.qian.mail@gmail.com>
2023-10-11 09:45:04 -0400

    added css styling for login and registration page

--- movie-rater/src/components/auth.js
+++ movie-rater/src/components/auth.js
@@ -6,19 +6,24 @@ function Auth() {
 
 	const [ username, setUsername ] = useState('');
 	const [ password, setPassword ] = useState('');
+	const [ confirmPassword, setConfirmPassword ] = useState('');
+	const [ email, setEmail ] = useState('');
 	const [ isLoginView, setIsLoginView] = useState(true);
 
 	const [ token, setToken ] = useCookies(['mr-token'])
 
 	useEffect( () => {
-		console.log(token);
 		if(token['mr-token']) window.location.href = '/movies';
 	}, [token])
 
 	const loginClicked = () => {
-		API.loginUser({username, password})
-			.then( resp => setToken('mr-token', resp.token))
-			.catch(error => console.log(error))
+		if (username && password) {
+			API.loginUser({username, password})
+				.then( resp => setToken('mr-token', resp.token))
+				.catch(error => console.log(error))
+		} else {
+			console.log("Login Error")
+		}
 	}
 
 	const handleKeyUp = e => {
@@ -28,41 +33,104 @@ function Auth() {
 	}
 
 	const registerClicked = () => {
-		API.registerUser({username, password})
-		.then( () => loginClicked())
-		.catch(error => console.log(error))
+		if (username && email && password && password === confirmPassword) {
+			API.registerUser({username, password})
+				.then( () => loginClicked())
+				.catch(error => console.log(error))
+		} else {
+			console.log("Please Check Input")
+		}
 	}
 
 	return (
-		<div>
-			{isLoginView ? <h1>Login</h1> : <h1>Register</h1>}
-			<label htmlFor="username">Username</label>
-			<br />
-			<input 
-				id="username" 
-				type="text" 
-				placeholder="username" 
-				value={username}
-				onChange={ evt => setUsername(evt.target.value)} />
-			<br />
-			<label htmlFor="password">Password</label>
-			<br />
-			<input 
-				id="password" 
-				type="password" 
-				placeholder="password" 
-				value={password}
-				onChange={ evt => setPassword(evt.target.value)} 
-				onKeyUp={handleKeyUp} />
-			<br/>
-			{isLoginView ? 
-				<button type="submit" onClick={loginClicked}>Login</button> : 
-				<button type="submit" onClick={registerClicked}>Register</button>
-			}
-			{isLoginView ?
-				<p onClick={() => setIsLoginView(false)}>Not registered? Create an Account</p> :
-				<p onClick={() => setIsLoginView(true)}>Already registered? Sign In!</p>
-			}
+		<div className="App">
+			<div className="login-container">
+				{ isLoginView ?
+					<>
+						<h1>Login</h1>
+						<br/>
+						<div className="user-box">
+							<input 
+								id="username" 
+								type="text"
+								value={username}
+								required=""
+								onChange={ evt => setUsername(evt.target.value)} />
+							<label htmlFor="username">Username</label>
+						</div>
+						<br/><br/>
+						<div className="user-box">
+							<input 
+								id="password" 
+								type="password" 
+								value={password}
+								required=""
+								onChange={ evt => setPassword(evt.target.value)} 
+								onKeyUp={handleKeyUp} />
+							<label htmlFor="password">Password</label>
+						</div>
+						<br/><br/>
+						<button className="btn" type="submit" onClick={loginClicked}>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							Login
+						</button>
+						<p>Don't have an account? <a onClick={() => setIsLoginView(false)} className="a-link">Signup</a></p>
+					</>
+					:
+					<>
+						<h1>Signup</h1>
+						<br/>
+						<div className="user-box">
+							<input 
+								id="username" 
+								type="text" 
+								value={username}
+								onChange={ evt => setUsername(evt.target.value)} />
+							<label htmlFor="username">Username</label><br/>
+						</div>
+						<br/>
+						<div className="user-box">
+							<input 
+								id="email" 
+								type="text"
+								value={email}
+								onChange={ evt => setEmail(evt.target.value)} />
+							<label htmlFor="email">Email</label><br/>
+						</div>
+						<br/>
+						<div className="user-box">
+							<input 
+								id="password" 
+								type="password"
+								value={password}
+								onChange={ evt => setPassword(evt.target.value)} />
+							<label htmlFor="password">Password</label>
+						</div>
+						<br/>
+						<div className="user-box">
+							<input 
+								id="confirmPassword" 
+								type="password"
+								value={confirmPassword}
+								onChange={ evt => setConfirmPassword(evt.target.value)} 
+								onKeyUp={handleKeyUp} />
+							<label htmlFor="confirmPassword">Confirm Password</label>
+						</div>
+						<br/><br/>
+						<button className="btn" type="submit" onClick={registerClicked}>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							Register
+						</button>
+						<p>Already have an account? <a onClick={() => setIsLoginView(true)} className="a-link">Login</a></p>
+					</>
+				}
+			</div>
 		</div>
 	)
 }
--- movie-rater/src/components/movie-list.js
+++ movie-rater/src/components/movie-list.js
@@ -24,7 +24,7 @@ function MovieList(props){
 
     return (
         <div>
-            { props.movies && props.movies.map( movie => {
+            { props?.movies && Array.isArray(props.movies) && props.movies.map( movie => {
                 return (
                 <div key={movie.id} className="movie-item">
                     <h2 onClick={movieClicked(movie)}>{movie.title}</h2>
